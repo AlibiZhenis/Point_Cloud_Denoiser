e769e3b201598c321f0254e7d8e60468b7abaaed
Alibi Zhenis <alibizhenis4@gmail.com>
2025-02-07 16:33:27 +0800

    Working Score-Based denoising

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 git clone https://github.com/AlibiZhenis/Point_Cloud_Denoiser.git
-conda install -c conda-forge -c davidcaron pclpy
+Install Visual Studio Build Tools 2019
+
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 conda install scipy plyfile scikit-learn matplotlib pillow=4.0.0 -c conda-forge
-Install Visual Studio Build Tools 2019
-pip install Cython chardet open3d
+conda install -c conda-forge tqdm scipy scikit-learn pyyaml easydict tensorboard pandas
+conda install -c conda-forge point_cloud_utils==0.18.0
+conda install -c fvcore -c iopath -c conda-forge fvcore iopath
 
-Install PointCloudLibrary
-https://python-pcl-fork.readthedocs.io/en/rc_patches4/install.html#install-python-pcl
-https://www.studyplan.dev/pro-cpp/vcpkg-windows
-
-conda install -c sirokujira pcl --channel conda-forge
+pip freeze
+pip install "git+https://github.com/facebookresearch/pytorch3d.git@stable"
+pip install [wheel filename]
 
 
 python Point_Cloud_Denoiser/Pointfilter/Customer_Module/chamfer_distance/setup.py install
 
 
 nvidia rtx a4000
+Windows 11
 cuda 11.3
 cudnn 8.2
 python 3.7
--- main.py
+++ main.py
@@ -2,65 +2,33 @@ import os
 # import pcl
 import numpy as np
 import torch
+import open3d as o3d
+from tqdm.auto import tqdm
 
 # from IO import IO
 from PointFilter.Pointfilter_Network_Architecture import pointfilternet
 from PointFilter.Pointfilter_DataLoader import PointcloudPatchDataset
 
+from score_denoise.utils.misc import *
+from score_denoise.utils.denoise import *
+from score_denoise.models.denoise import *
+
 file_dir = os.path.dirname(os.path.abspath(__file__)) 
 
 class PC_denoiser:
-    # @staticmethod
-    # def denoise_bilateral_filter(input_object, distance_sigma=0.1, normal_radius=0.2, spatial_sigma=0.1, output_file = None):
-    #     cloud = IO.get_pcl_from(input=input_object)
-    #     filter = cloud.make_bilateral_filter()
-
-    #     # Set the parameters for the filter
-    #     filter.set_DistanceSigma(distance_sigma) 
-    #     filter.set_NormalRadius(normal_radius)  
-    #     filter.set_SpatialSigma(spatial_sigma) 
-
-    #     # Apply the filter
-    #     filtered_cloud = filter.filter()
-
-    #     if output_file:
-    #         pcl.save(filtered_cloud, output_file)
-        
-    #     return filtered_cloud
-    
-    # @staticmethod
-    # def denoise_voxel_grid_filter(input_object, leaf_size=0.1, output_file = None):
-    #     cloud = IO.get_pcl_from(input=input_object)
-    #     filter = cloud.make_voxel_grid_filter()
-
-    #     # Set the parameters for the filter
-    #     filter.set_leaf_size(leaf_size, leaf_size, leaf_size)
-
-    #     # Apply the filter
-    #     filtered_cloud = filter.filter()
-
-    #     if output_file:
-    #         pcl.save(filtered_cloud, output_file)
-        
-    #     return filtered_cloud
-    
     @staticmethod
     def denoise_pointfilter(cloud, patch_radius = 0.05, num_workers = 0, model_path = None, output_file = None):
-        # cloud = IO.get_pcl_from(input=input_object)
-
         test_dataset = PointcloudPatchDataset(
             cloud=cloud,
             patch_radius=patch_radius,
             train_state='evaluation',
             shape_name="dummy")
-        # print(len(test_dataset))
         test_dataloader = torch.utils.data.DataLoader(
             test_dataset,
             num_workers=int(num_workers),
             batch_size = 64,
             )
-        # print(len(test_dataloader))
-        
+                
         pointfilter_eval = pointfilternet().cuda()
         if model_path is None:
             model_path = os.path.join(file_dir, 'PointFilter\Summary\pre_train_model\model_full_ae.pth')
@@ -73,26 +41,53 @@ class PC_denoiser:
         pred_pts = np.empty((0, 3), dtype='float32')
         for batch_ind, data_tuple in enumerate(test_dataloader):
             noise_patch, noise_inv, noise_disp = data_tuple
-            # print(noise_patch.shape, noise_inv.shape, noise_disp.shape)
             noise_patch = noise_patch.float().cuda()
             noise_inv = noise_inv.float().cuda()
             noise_patch = noise_patch.transpose(2, 1).contiguous()
-            # print(noise_patch.shape, noise_inv.shape, noise_disp.shape)
             predict = pointfilter_eval(noise_patch)
             predict = predict.unsqueeze(2)
             predict = torch.bmm(noise_inv, predict)
             pred_pts = np.append(pred_pts,
                                     np.squeeze(predict.data.cpu().numpy()) * patch_radius + noise_disp.numpy(),
                                     axis=0)
-        # np.save(os.path.join(opt.save_dir, shape_name + '_pred_iter_' + str(eval_index + 1) + '.npy'),
-        #     pred_pts.astype('float32'))
 
 
         if output_file:
-            # pcl.save(pred_pts.astype('float32'), output_file)
             np.save(output_file, pred_pts.astype('float32'))
         
         return pred_pts
+    
+    @staticmethod
+    def denoise_score_based(cloud, cluster_size=30000, model_path = None, output_file=None):
+        if model_path is None:
+            model_path = os.path.join(file_dir, "score_denoise\pretrained\ckpt.pt")
+        device = "cuda"
+
+        ckpt = torch.load(model_path, map_location=device)
+        model = DenoiseNet(ckpt['args']).to(device)
+        model.load_state_dict(ckpt['state_dict'])
+
+        cloud = torch.FloatTensor(cloud)
+
+        if cloud.size(0) <= 50000:
+            pcl, center, scale = NormalizeUnitSphere.normalize(cloud)
+            pcl = pcl.to(device)
+            pcl_denoised = patch_based_denoise(model, pcl).cpu()
+            pcl_denoised = pcl_denoised * scale + center
+        else:
+            pcl = cloud.to(device)
+            pcl_denoised = denoise_large_pointcloud(
+                model=model,
+                pcl=pcl,
+                cluster_size=cluster_size,
+                seed=2025,
+            )
+            pcl_denoised = pcl_denoised.cpu().numpy()
+
+        if output_file:
+            np.save(output_file, pcl_denoised.astype('float32'))
+        
+        return pcl_denoised
         
 # Example usage
 # arr = np.load("data/Tetrahedron.npy")
@@ -101,23 +96,8 @@ output_file = 'denoised_point_cloud.pcd'
 
 arr = np.load(input_file)
 print(arr.shape)
-arr = PC_denoiser.denoise_pointfilter(arr, output_file=output_file)
+arr = PC_denoiser.denoise_score_based(arr, output_file=output_file)
 print(arr.shape)
-# arr = IO.pcl_to_numpy(cloud=arr)
-
-# import matplotlib.pyplot as plt
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(arr[:, 0], arr[:, 1], arr[:, 2])
-
-# ax.set_xlabel('X Label')
-# ax.set_ylabel('Y Label')
-# ax.set_zlabel('Z Label')
-
-# plt.show()
-
-import open3d as o3d
-import numpy as np
 
 # Create a sample point cloud
 point_cloud = o3d.geometry.PointCloud()
